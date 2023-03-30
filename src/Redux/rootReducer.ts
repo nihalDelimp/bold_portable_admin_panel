692dd22fea39f68c8dcf1d34554dfd24cc5b6497
tahseem <tahseem@delimp.com>
2023-03-30 17:48:52 +0530

    crear state on log out

--- src/Redux/rootReducer.ts
+++ src/Redux/rootReducer.ts
@@ -1,11 +1,19 @@
-import { combineReducers } from '@reduxjs/toolkit';
+import { combineReducers ,AnyAction } from '@reduxjs/toolkit';
 import auth from './Reducers/authSlice';
 import notification from './Reducers/notificationSlice';
 
-const rootReducer = combineReducers({
+const appReducer = combineReducers({
     auth ,
     notification
 })
+
+const rootReducer = (state : any, action : AnyAction ) => {
+    if (action.type === 'auth/logout') {
+        return appReducer(undefined, action);
+    }
+    return appReducer(state, action)
+}
+
 
 export type RootState = ReturnType<typeof rootReducer>
 
