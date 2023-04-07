a2f4e63d064366414dfcc743cf291f4a2150008c
tahseem <tahseem@delimp.com>
2023-04-07 15:23:42 +0530

    worked on notification

--- src/Common/Notification.tsx
+++ src/Common/Notification.tsx
@@ -4,24 +4,37 @@ import { RootState } from "../Redux/rootReducer";
 import { authAxios } from "../config/config";
 import { toast } from "react-toastify";
 import { addNewOrderMsg } from "../Redux/Reducers/notificationSlice";
+import IsLoadingHOC from "./IsLoadingHOC";
 import io, { Socket } from "socket.io-client";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 dayjs.extend(relativeTime);
 
-const Notification = () => {
+interface MyComponentProps {
+  setLoading: (isComponentLoading: boolean) => void;
+}
+
+const Notification = (props: MyComponentProps) => {
+  const { setLoading } = props;
+  const [allNotifucations, setAllNotifications] = useState<any[]>([]);
+  const [totalCount, setTotalCount] = useState<number>(0);
   const dispatch = useDispatch();
   const socket = useRef<Socket>();
   const { newOrdersMsg } = useSelector(
     (state: RootState) => state.notification
   );
+
+  console.log("AllNotification", allNotifucations);
+  console.log("TotalCont", totalCount);
+
   socket.current = io(`${process.env.REACT_APP_SOCKET}`);
 
   useEffect(() => {
     if (socket.current) {
       socket.current.on("new_order_recieved", (recieved_order) => {
         console.log("recieved_order", recieved_order);
-        dispatch(addNewOrderMsg(recieved_order));
+        // dispatch(addNewOrderMsg(recieved_order));
+        getAllNotifications();
       });
     }
     return () => {
@@ -29,28 +42,75 @@ const Notification = () => {
     };
   }, []);
 
-  // const getCustomerDetailsData = async (orderDetail: any) => {
-  //   let placedOrder = orderDetail;
-  //   console.log(orderDetail);
-  //   await authAxios()
-  //     .get(`/auth/get-specific-user/${orderDetail.user}`)
-  //     .then(
-  //       (response) => {
-  //         if (response.data.status === 1) {
-  //           const resData = response.data.data;
-  //           console.log(resData, "resDataresDataresDataresData");
-  //           placedOrder["userName"] = resData.name;
-  //           dispatch(addNewOrderMsg(placedOrder));
-  //         }
-  //       },
-  //       (error) => {
-  //         toast.error(error.response.data.message);
-  //       }
-  //     )
-  //     .catch((error) => {
-  //       console.log("errorrrr", error);
-  //     });
-  // };
+  useEffect(() => {
+    getAllNotifications();
+  }, []);
+
+  const getAllNotifications = async () => {
+    await authAxios()
+      .get(`/notification/get-all-unseen-notfications`)
+      .then(
+        (response) => {
+          if (response.data.status === 1) {
+            const resData = response.data.data;
+            setTotalCount(response.data?.count);
+            console.log("Notifications_resData", resData);
+            setAllNotifications(resData);
+          }
+        },
+        (error) => {
+          toast.error(error.response.data.message);
+        }
+      )
+      .catch((error) => {
+        console.log("errorrrr", error);
+      });
+  };
+
+  const markAllNotificationsSeen = async () => {
+    setLoading(true);
+    await authAxios()
+      .put(`/notification/mark-all-notfications-true`)
+      .then(
+        (response) => {
+          setLoading(false);
+          if (response.data.status === 1) {
+            const resData = response.data.data;
+            console.log("MarkReadALlNotifications_resData", resData);
+            getAllNotifications();
+          }
+        },
+        (error) => {
+          setLoading(false);
+          toast.error(error.response.data.message);
+        }
+      )
+      .catch((error) => {
+        console.log("errorrrr", error);
+      });
+  };
+
+  const markSpecificNotificationSeen = async (_id: string) => {
+    setLoading(true);
+    await authAxios()
+      .patch(`/notification/${_id}/mark-specific-notification-as-seen`)
+      .then(
+        (response) => {
+          setLoading(false);
+          if (response.data.status === 1) {
+            toast.success(response.data.message);
+            getAllNotifications();
+          }
+        },
+        (error) => {
+          setLoading(false);
+          toast.error(error.response.data.message);
+        }
+      )
+      .catch((error) => {
+        console.log("errorrrr", error);
+      });
+  };
 
   console.log("OrderNotification", newOrdersMsg);
 
@@ -63,7 +123,7 @@ const Notification = () => {
       >
         <div
           className={
-            newOrdersMsg && newOrdersMsg.length > 0
+            allNotifucations && allNotifucations.length > 0
               ? "icon-status icon-status-info"
               : ""
           }
@@ -74,33 +134,40 @@ const Notification = () => {
       <div className="dropdown-menu dropdown-menu-xl dropdown-menu-end">
         <div className="dropdown-head">
           <span className="sub-title nk-dropdown-title">Notifications</span>
-          <a href="#">Mark All as Read</a>
+          <a href="#" onClick={markAllNotificationsSeen}>Mark All as Read</a>
         </div>
         <div className="dropdown-body">
           <div className="nk-notification">
-            {newOrdersMsg &&
-              newOrdersMsg.length > 0 &&
-              newOrdersMsg
-                .slice(0)
-                .reverse()
-                .map((item: any, index: number) => (
-                  <div
-                    key={index + 1}
-                    className="nk-notification-item dropdown-inner"
-                  >
-                    <div className="nk-notification-icon">
-                      <em className="icon icon-circle bg-warning-dim ni ni-curve-down-right"></em>
-                    </div>
-                    <div className="nk-notification-content">
-                      <div className="nk-notification-text">
-                        {`${item.user.name} has Placed an order`}
+            {allNotifucations &&
+              allNotifucations.length > 0 &&
+              allNotifucations.map((item: any, index: number) => {
+                if (!item.status_seen) {
+                  return (
+                    <div
+                      key={index + 1}
+                      className="nk-notification-item dropdown-inner"
+                    >
+                      <div className="nk-notification-icon">
+                        <a
+                          onClick={() => markSpecificNotificationSeen(item._id)}
+                        >
+                          <em className="icon icon-circle bg-warning-dim ni ni-curve-down-right"></em>
+                        </a>
                       </div>
-                      <div className="nk-notification-time">
-                        <span>{dayjs(item.createdAt).fromNow()}</span>
+                      <div className="nk-notification-content">
+                        <div className="nk-notification-text">
+                          {`${item.user.name} has Placed an order`}
+                        </div>
+                        <div className="nk-notification-time">
+                          <span>{dayjs(item.createdAt).fromNow()}</span>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                ))}
+                  );
+                } else {
+                  return null;
+                }
+              })}
           </div>
         </div>
         <div className="dropdown-foot center">
@@ -111,4 +178,4 @@ const Notification = () => {
   );
 };
 
-export default Notification;
+export default IsLoadingHOC(Notification);
