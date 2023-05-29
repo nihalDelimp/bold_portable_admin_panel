428216fe14985588ae38c12bedc7dda071996a5c
tahseem <tahseem@delimp.com>
2023-05-29 18:44:44 +0530

    fixed redux store error

--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -1,10 +1,19 @@
-import { configureStore, Action } from '@reduxjs/toolkit';
+import { configureStore, Action , getDefaultMiddleware } from '@reduxjs/toolkit';
 import { ThunkAction } from 'redux-thunk';
 import { useDispatch } from 'react-redux';
 import logger from 'redux-logger'
 import rootReducer, { RootState } from './rootReducer';
 import storage from "redux-persist/lib/storage";
-import { persistStore, persistReducer } from "redux-persist";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
 
 const persistConfig = {
@@ -12,11 +21,17 @@ const persistConfig = {
   storage: storage,
 };
 
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
   reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+ // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 })
 
 if (process.env.NODE_ENV === 'development' && module.hot) {
