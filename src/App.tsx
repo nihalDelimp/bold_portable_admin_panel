a21828204ff25627ab2bb4e552b58a22d92bf3a9
shivakar <shivakar@delimp.com>
2023-07-27 14:15:55 +0530

    remove custom jquery cdn

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ function App() {
   return (
     <div className="nk-app-root">
       <div className="nk-main">
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
         <RootRouter />
         <ToastContainer autoClose={6000} position="top-right" />
       </div>
